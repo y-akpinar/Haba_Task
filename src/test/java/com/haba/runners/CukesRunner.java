0702f8898d374ad87b53d3f594d147269445a1ea
yasinakpnr <akpinaryasin1991@gmail.com>
2022-05-02 23:53:53 +0200

    update dependency and add pretty report plugin

--- src/test/java/com/haba/runners/CukesRunner.java
+++ src/test/java/com/haba/runners/CukesRunner.java
@@ -7,13 +7,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin= {"json:target/cucumber.json",},
+        plugin= {"json:target/cucumber.json",
+                "me.jvt.cucumber.report.PrettyReports:target"},
         features = "src/test/resources/features",
         glue = "com/haba/step_definitions",
         dryRun = false,
-        tags = ""
+        tags = "@wip"
 )
 
 
 public class CukesRunner {
 }
+
